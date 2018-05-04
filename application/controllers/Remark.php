eb13bd781e0d8d373eab8042837bb2fef7482942
brightming <minghgu@163.com>
2018-05-04 22:54:01 +0800

    remark add

--- application/controllers/Remark.php
+++ application/controllers/Remark.php
@@ -17,25 +17,29 @@ public function addRemark(){
 		$rws_post = $GLOBALS['HTTP_RAW_POST_DATA'];
 		$mypost = json_decode($rws_post);
 		
-		/*
-		$desc=$mypost['extraDesc'];
-		$scores=$mypost['scores'];
-		$storeId=$mypost['storeId'];
-		$tableId=$mypost['tableId'];
-		$openId=$mypost['openId'];
+		
+		$desc=$mypost->extraDesc;
+		$scores=$mypost->scores;
+		$storeId=$mypost->storeId;
+		$tableId=$mypost->tableId;
+		$openId=$mypost->openId;
 		
 		
 		//查看这个openid的用户，对于这个店的这个桌子的点评，最近的一次是在什么时候，如果相隔不超过1小时，则拒绝评论
 		$latest=remarkModel::getUserLatestRemark($openId,$storeId,$tableId);
 		if($latest==NULL){
 			//可以写
+			foreach($mypost->scores as $score){
+				print_r($score->remarkTempId);
+	            //remarkModel::addUserLatestRemark($mypost);
+			}
 			
 		}else{
 			//判断时间差是否满足要求，如果时间距离太近，则认为是已经点评过了，不允许再点评
 			
 			
 		}
-		*/
+		/**/
 			$this->json([
 			'uri'=>$uri,
 			'openId'=>$this->input->post('openId'),
