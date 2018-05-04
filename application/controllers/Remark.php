7d108b2a12e87daa9bf1e75f72e96b7d6aa066a3
brightming <minghgu@163.com>
2018-05-04 22:30:31 +0800

    remark test

--- application/controllers/Remark.php
+++ application/controllers/Remark.php
@@ -17,11 +17,13 @@ public function addRemark(){
 		$rws_post = $GLOBALS['HTTP_RAW_POST_DATA'];
 		$mypost = json_decode($rws_post);
 		
+		/*
 		$desc=$mypost['extraDesc'];
 		$scores=$mypost['scores'];
 		$storeId=$mypost['storeId'];
 		$tableId=$mypost['tableId'];
 		$openId=$mypost['openId'];
+		
 		
 		//查看这个openid的用户，对于这个店的这个桌子的点评，最近的一次是在什么时候，如果相隔不超过1小时，则拒绝评论
 		$latest=remarkModel::getUserLatestRemark($openId,$storeId,$tableId);
@@ -33,7 +35,7 @@ public function addRemark(){
 			
 			
 		}
-		
+		*/
 			$this->json([
 			'uri'=>$uri,
 			'openId'=>$this->input->post('openId'),
