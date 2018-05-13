bc4b7e9b9a06997493fd097898f36fa8494b8a19
brightming <minghgu@163.com>
2018-05-13 16:08:35 +0800

    draw money

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 use \QCloud_WeApp_SDK\Auth\LoginService as LoginService;
 use QCloud_WeApp_SDK\Constants as Constants;
+use \QCloud_WeApp_SDK\Model\Coupon as CouponModel;
 
 class User extends CI_Controller {
     public function index() {
@@ -20,5 +21,62 @@ class User extends CI_Controller {
             ]);
         }
     }
+	
+	public function getMyCoupon(){
+		
+		$openId=$_POST["openId"];	
+		$totalCouponCnt=$_POST["totalCouponCnt"];//总数量，如果是第一次获取，此参数是负数，那么就需要先获取总数量
+		$startIdx=$_POST["startIdx"];//分页查询的起始编号
+		$needCnt=$_POST["needCnt"];//获取多少条
+		
+		if($totalCouponCnt<0){
+			
+		}
+		
+		
+		
+	}
+
+	/**
+	* 判断某用户此时是否能抽奖
+	*/
+    public function canDrawMoney(){
+         //token是判断的依据
+		$rws_post = $GLOBALS['HTTP_RAW_POST_DATA'];
+		$mypost = json_decode($rws_post);
+		
+		
+		$token=$mypost->token;
+		$storeId=$mypost->storeId;
+		$tableId=$mypost->tableId;
+		$openId=$mypost->openId;
+		
+		$userinfo = User::findUserByOpenId($openId);
+		
+		//获取最新的token
+		$setting=CouponModel::getDrawCash($storeId);
+		if($setting==NULL){
+			//此店未设置抽奖
+			 $this->json([
+                'code' => -1,
+                'desc' =>'此店未设置抽奖'
+            ]);
+			return;
+		}
+		
+		//对比token
+		if($token!=$setting->token){
+			//用了过时的token
+			 $this->json([
+                'code' => -2,
+                'desc' =>'无效凭证'
+            ]);
+			return;
+			
+		}
+		
+
+
+    }	
 
 }
---
+++ vendor/qcloud/weapp-sdk/lib/Model/Coupon.php
@@ -0,0 +1,75 @@
+<?php
+namespace QCloud_WeApp_SDK\Model;
+
+use QCloud_WeApp_SDK\Mysql\Mysql as DB;
+use QCloud_WeApp_SDK\Constants;
+use \Exception;
+
+class Coupon
+{
+    public static function countCoupon ($customer_id) {
+       // 拼接 SQL 语句 ，统计各种状态的优惠券的数量
+	   $conditions=['customer_id'=>$customer_id];
+	   $operator='and';
+	   // 处理条件
+        list($condition, $execValues) = array_values(self::conditionProcess($conditions, $operator));
+
+        // 拼接 SQL 语句
+        $sql = "select * FROM `CustomerCoupon` WHERE $condition ";
+
+        // 执行 SQL 语句
+        $query = self::raw($sql, $execValues);
+        return $query->rowCount();
+    }
+
+	/**
+	*  分页获取获取某用户，某种状态的优惠券
+	*/
+	public static function getCouponByPage($customer_id,$coupon_status,$startIdx,$pageCnt){
+		
+		
+		
+		
+	}
+	
+		
+	/**
+	* 分配优惠券给用户
+	*/
+	public static function assignConpon($customer_id,$coupon_data){
+		
+		
+		
+	}
+	
+	/**
+	*  记录用户的抽奖行为
+	*/
+	public static function noteDownUserDrawCashRec($customer_id,$other_data){
+		$create_time=date('Y-m-d H:i:s');
+		DB::insert('CacheDrawRecord', compact('customer_id', 'create_time'));
+	}
+	
+	/**
+	* 将用户抽中的现金的记录进行记录
+	*/
+	public static function assignCacheToUser($customer_id,$money_amount,$sent,$store_id){
+		
+		$create_time=date('Y-m-d H:i:s');
+		DB::insert('CashAssignRecord', compact('customer_id', 'create_time','sent','store_id','money_amount'));
+		
+	}
+  
+    
+	
+	/**
+	* 使用优惠券
+	*
+	*/
+	public static function useCouponStatus($customer_id,$couponId,$options){
+		
+		
+		
+	}
+
+}
