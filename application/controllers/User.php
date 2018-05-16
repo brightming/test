ef460184fdf1f56cea0b27283909e3fc358a87b4
brightming <minghgu@163.com>
2018-05-16 21:28:12 +0800

    gg

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 use \QCloud_WeApp_SDK\Auth\LoginService as LoginService;
 use QCloud_WeApp_SDK\Constants as Constants;
 use \QCloud_WeApp_SDK\Model\Coupon as CouponModel;
+use \QCloud_WeApp_SDK\Model\User as User;
 
 class User extends CI_Controller {
     public function index() {
