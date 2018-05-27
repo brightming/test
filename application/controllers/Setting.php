bc0367c77844f24bd999b9126d9a7386608ed3aa
sunny <minghgu@163.com>
2018-05-27 12:37:37 +0800

    gg

--- application/controllers/Setting.php
+++ application/controllers/Setting.php
@@ -18,7 +18,15 @@ class Setting extends CI_Controller {
     public function check_remark_setting() {
         $met= $this->input->method();
         if(strcasecmp($met,"post")!=0){
-            $this->json(["code"=>600,"msg"=>"check_remark_setting"]);
+            $this->json(["code"=>600,"msg"=>"check_remark_setting.expected post method"]);
+            //return;
+        }
+        $lng=$this->input->post('lng');
+        $lat=$this->input->post('lat');
+        $unionId=$this->input->post('unionId');
+        
+        if($lng==NULL || $lat==NULL || $unionId==NULL){
+            $this->json(["code"=>-6,"msg"=>"check_remark_setting.param not enough"]);
             return;
         }
     }
